930879a9286beaa893312c94cbc76429d240301b
Leo-droid-maker <jwabeljr@gmail.com>
2025-07-22 19:14:07 +0100

    deck init

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,5 +5,35 @@ using UnityEngine;
 
 namespace LeoDroidMakerObjects
 {
+    [CreateAssetMenu(fileName = "new Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardType;
+        public int health;
+        public int damageMin;
+        public int damageMax;
+        public Sprite cardSprite;
+        public List<DamageType> damageType;
 
+        public enum CardType
+        {
+            Fire,
+            Earth,
+            Water,
+            Dark,
+            Light,
+            Air
+        }
+
+        public enum DamageType
+        {
+            Fire,
+            Earth,
+            Water,
+            Dark,
+            Light,
+            Air
+        }
+    }
 }
